c19d12ffd0781b64c31f58a65ce35a8449c5d835
Nikita Dorokhov <nktdrkhv@outlook.com>
2023-10-01 21:52:10 +0700

    CaptionAttribute filter

---
+++ Src/TelegramUpdater/FilterAttributes/Attributes/CaptionAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace TelegramUpdater.FilterAttributes.Attributes;
+
+/// <summary>
+/// Filter attibute for <see cref="FilterCutify.Caption"/>
+/// </summary>
+public sealed class CaptionAttribute : FilterAttributeBuilder
+{
+    /// <summary>
+    /// Initialize a new instance of <see cref="CaptionAttribute"/>.
+    /// </summary>
+    public CaptionAttribute()
+        : base(x => x.AddFilterForUpdate(FilterCutify.Caption()))
+    {
+    }
+}
